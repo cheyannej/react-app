c75ff3f56d03e88639daf25e5b3963b46bc59638
Cheyanne Jackson <queenchey@Cheyannes-MacBook-Pro.local>
2019-08-13 18:26:10 -0400

    React Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() { //returns DOM reference
+      let txt = this.props.txt
+      return <h1>{txt}</h1>
+    }
+  }
+
+App.propTypes = { //defining properties
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
 }
 
-export default App;
+App.defaultProps = {
+  txt: "this is the default txt"
+}
+
+export default App
