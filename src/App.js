99ffa1aa35514bdf58402cd942730447b540db25
Cheyanne Jackson <queenchey@Cheyannes-MacBook-Pro.local>
2019-08-13 19:35:08 -0400

    React - Lifecylce methods

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 
 //reference a node or instance of a component
 
 class App extends React.Component {
   constructor() {
     super(); //gets context
-    this.state = {a: ''}
+    this.state = {value: 0}
+    this.update = this.update.bind(this)
   }
 
   update() {
-    this.setState({
-      a: this.a.refs.input.value,
-      b: this.refs.b.value
-    })
+    this.setState({val: this.state.val + 1})
+  }
+
+  componentWillMount() { //right before fired into DOm
+    console.log('componentWillMount')
+  }
+
+  render() {
+    console.log('render');
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+
+  componentDidMount() { //component is mounted to dom
+      console.log('componentDidMount')
+  }
+
+
+  componentWillUnmount() { //component is mounted to dom
+        console.log('componentWillUnmount')
+      }
+    }
+
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
   }
 
   render() {
     return (
       <div>
-        <Input
-          ref={component => this.a = component}
-          update={this.update.bind(this)}
-          /> {this.state.a}
-
-          <hr />
-
-          <input
-            ref="b"
-            type="text"
-            onChange={this.update.bind(this)}
-            /> {this.state.b}
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id="a"></div>
       </div>
     )
   }
 }
 
-  class Input extends React.Component {
-    render() {
-      return <div><input ref="input" type="text" onChange={this.props.update}/></div>
-    }
-  }
 
-
-export default App
+export default Wrapper
